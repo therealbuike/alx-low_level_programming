eb44cdf08c9b13d557f92ffd53e797f2b282a8f9
therealbuike <bukkyokerulu@yahoo.com>
2023-02-21 13:32:20 -0800

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print the alpha in lowercase
+ *
+ * Return: Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= '2'; la++)
+		_putchar(la);
+
+	_putchar('\n');
