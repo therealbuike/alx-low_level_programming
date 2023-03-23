a730a4a0d6cdea6210be53eddb9123d31b4dd0ba
therealbuike <bukkyokerulu@yahoo.com>
2023-03-23 11:50:54 -0700

    task4

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,12 +11,9 @@ void print_all(const char * const format, ...)
 	int i = 0;
 	char *str, *sep = "";
 
-
 	va_list list;
 
-
 	va_start(list, format);
-
 
 	if (format)
 	{
@@ -47,7 +44,6 @@ void print_all(const char * const format, ...)
 			i++;
 		}
 	}
-
 
 	printf("\n");
 	va_end(list);
